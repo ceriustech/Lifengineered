54b5897ea7d113002054a6c301da2a3cd83a470c
Bill <ceriustech@gmail.com>
2020-05-18 23:49:10 -0500

    The footer will be loaded across the site with javascript.

--- assets/js/helper-scripts.js
+++ assets/js/helper-scripts.js
@@ -1,5 +1,5 @@
 // This function loads the Nav bar across the site
-let navPlacementFunc = (() => {
+const navPlacementFunc = (() => {
     let navBar = document.getElementById('navigation'); 
 console.log(navBar);
     let navHTML; 
@@ -61,6 +61,130 @@ if(window.location.href.includes("articles")) {
 navBar.innerHTML = navHTML; 
 })(); 
 
+// This function will load the footer accross the site
+
+const footerPlacementFunc = (() => {
+    let footer = document.getElementById('footer');
+    console.log(footer);
+    
+    let footerHTML; 
+    
+    let footerEl = `<div class="inner">
+					<section class="info">
+					    <div class="footer-info__heading">
+					       <h3 class="logo footer-info__h3">Lif<span class="logo-txt-color">E</span>ngineered</h3> 
+					    </div>
+					    <div class="footer-link-container">
+					       <div class="about">
+							    <div class="col-6 col-12-small">
+				                    <ul class="alt">
+								        <li><a href="#" class="name">About Us</a></li>
+								        <li><a href="#" class="name">Contact Us</a></li>
+								    </ul>
+				                </div>
+						    </div>
+						  <div class="footer-nav">
+							<div class="footer-nav-item__container">
+								<a href="#" class="name">
+									Home
+								</a>
+							</div>
+							<div class="footer-nav-item__container">
+								<a href="#" class="name">
+									Technology
+								</a>
+							</div>
+							<div class="footer-nav-item__container">
+								<a href="#" class="name">
+									Entrepreneurship
+								</a>
+							</div>
+							<div class="footer-nav-item__container">
+								<a href="#" class="name">
+									Health
+								</a>
+							</div>
+						  </div>
+					    </div>
+						
+						
+					</section>
+					<section class="contact">
+						<h3 class="contact-heading"><span class="left-footer-dash">-</span> Social Media <span class="right-footer-dash">-</span></h3>
+						<ul class="icons">
+				            <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+				            <li><a href="#" class="icon brands fa-facebook"><span class="label">Facebook</span></a></li>
+				            <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+				            <li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+				        </ul>
+					</section>
+				</div>
+				<div class="copyright">
+					&copy; LifEngineered. ©2019 LimbleMedia. All rights reserved.
+				</div>`;
+    
+    let articleFooter = `<div class="inner">
+					<section class="info">
+					    <div class="footer-info__heading">
+					       <h3 class="logo footer-info__h3">Lif<span class="logo-txt-color">E</span>ngineered</h3> 
+					    </div>
+					    <div class="footer-link-container">
+					       <div class="about">
+							    <div class="col-6 col-12-small">
+				                    <ul class="alt">
+								        <li><a href="#" class="name">About Us</a></li>
+								        <li><a href="#" class="name">Contact Us</a></li>
+								    </ul>
+				                </div>
+						    </div>
+						  <div class="footer-nav">
+							<div class="footer-nav-item__container">
+								<a href="#" class="name">
+									Home
+								</a>
+							</div>
+							<div class="footer-nav-item__container">
+								<a href="#" class="name">
+									Technology
+								</a>
+							</div>
+							<div class="footer-nav-item__container">
+								<a href="#" class="name">
+									Entrepreneurship
+								</a>
+							</div>
+							<div class="footer-nav-item__container">
+								<a href="#" class="name">
+									Health
+								</a>
+							</div>
+						  </div>
+					    </div>
+						
+						
+					</section>
+					<section class="contact">
+						<h3 class="contact-heading"><span class="left-footer-dash">-</span> Social Media <span class="right-footer-dash">-</span></h3>
+						<ul class="icons">
+				            <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+				            <li><a href="#" class="icon brands fa-facebook"><span class="label">Facebook</span></a></li>
+				            <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+				            <li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+				        </ul>
+					</section>
+				</div>
+				<div class="copyright">
+					&copy; LifEngineered. ©2019 LimbleMedia. All rights reserved.
+				</div>`;
+    
+if(window.location.href.includes("articles")) {
+    footerHTML = articleFooter;
+}else {
+    footerHTML = footerEl; 
+}
+
+footer.innerHTML = footerHTML; 
+})();
 
 
 
