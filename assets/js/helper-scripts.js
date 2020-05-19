46c05f35c709062de044621449f7ce4f37674528
Bill <ceriustech@gmail.com>
2020-05-18 22:14:42 -0500

    Fixed a bug with the page routing.

--- assets/js/helper-scripts.js
+++ assets/js/helper-scripts.js
@@ -1,9 +1,10 @@
 // This function loads the Nav bar across the site
 let navPlacementFunc = (() => {
     let navBar = document.getElementById('navigation'); 
-console.log(navBar); 
+console.log(navBar);
+    let navHTML; 
 
-let navHTML = ` <div id="nav-wrapper" class="wrapper">
+let nav = ` <div id="nav-wrapper" class="wrapper">
             <header id="header">
 				<!-- Logo -->
 					<span class="logo">
@@ -26,6 +27,36 @@ let navHTML = ` <div id="nav-wrapper" class="wrapper">
              
 			</header>
         </div>`;
+
+let articleNav = `<div id="nav-wrapper" class="wrapper">
+		        <header id="header">
+				<!-- Logo -->
+					<span class="logo">
+						<a href="../../index.html" class="header-logo_a">Lif<span class="logo-txt-color">E</span>ngineered</a>
+					</span>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul class="header-nav_ul">
+							<li class="header-nav_li"><a href="../../index.html" class="header-nav_a">Home</a></li>
+							<li class="header-nav_li"><a href="../../lifestyle.html" class="header-nav_a">Lifestyle</a></li>
+							<li class="header-nav_li">
+								<a href="../../technology.html" class="nav-tem header-nav_a">Technology</a>
+							</li>
+							<li class="header-nav_li"><a href="../../entrepreneurship.html" class="header-nav_a">Entrepreneurship </a></li>
+							<li class="header-nav_li"><a href="../../health.html" class="header-nav_a">Health</a></li>
+							<!--<li><a href="#" class="button">Log In</a></li>-->
+						</ul>
+					</nav>
+             
+			</header>
+            </div>`;
+
+if(window.location.href.includes("articles")) {
+    navHTML = articleNav;
+}else {
+    navHTML = nav; 
+}
 
 navBar.innerHTML = navHTML; 
 })(); 
