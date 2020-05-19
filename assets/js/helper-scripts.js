b75eb668b119915df73e3e3bb274accf159a157d
Bill <ceriustech@gmail.com>
2020-05-18 21:54:36 -0500

    Created a function so the navigation will be loaded by javascript.

--- assets/js/helper-scripts.js
+++ assets/js/helper-scripts.js
@@ -1,6 +1,35 @@
-let postImg = document.getElementById('lifestyle-img-1');
-console.log(postImg);
+// This function loads the Nav bar across the site
+let navPlacementFunc = (() => {
+    let navBar = document.getElementById('navigation'); 
+console.log(navBar); 
 
-postImg.src = "../images/choices-lifestyle.jpg";
+let navHTML = ` <div id="nav-wrapper" class="wrapper">
+            <header id="header">
+				<!-- Logo -->
+					<span class="logo">
+						<a href="index.html" class="header-logo_a">Lif<span class="logo-txt-color">E</span>ngineered</a>
+					</span>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul class="header-nav_ul">
+							<li class="header-nav_li"><a href="index.html" class="header-nav_a">Home</a></li>
+							<li class="header-nav_li"><a href="lifestyle.html" class="header-nav_a">Lifestyle</a></li>
+							<li class="header-nav_li">
+								<a href="technology.html" class="nav-tem header-nav_a">Technology</a>
+							</li>
+							<li class="header-nav_li"><a href="entrepreneurship.html" class="header-nav_a">Entrepreneurship </a></li>
+							<li class="header-nav_li"><a href="health.html" class="header-nav_a">Health</a></li>
+							<!--<li><a href="#" class="button">Log In</a></li>-->
+						</ul>
+					</nav>
+             
+			</header>
+        </div>`;
+
+navBar.innerHTML = navHTML; 
+})(); 
+
+
 
 
